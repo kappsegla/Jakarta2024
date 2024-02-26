8e41335543589553c5b9296671e619617d85eb81
Martin Blomberg <martin.blomberg@outlook.com>
2024-02-26 20:58:21 +0100

    Remove swagger

--- src/main/java/se/iths/restdemo/HelloApplication.java
+++ src/main/java/se/iths/restdemo/HelloApplication.java
@@ -1,7 +1,5 @@
 package se.iths.restdemo;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
 import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
 
