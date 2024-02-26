e99a998ab0f5765b42b5c6988e92d891148cebf4
Martin Blomberg <martin.blomberg@outlook.com>
2024-02-26 20:49:20 +0100

    Add swagger

--- src/main/java/se/iths/restdemo/HelloApplication.java
+++ src/main/java/se/iths/restdemo/HelloApplication.java
@@ -1,5 +1,7 @@
 package se.iths.restdemo;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
 
